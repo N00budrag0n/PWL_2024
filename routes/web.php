d35f34ec97fbf6f3b30a4da0025c292ba75d199f
N00budrag0n <helmiagung2468@gmail.com>
2024-02-22 15:49:49 +0700

    praktikum bagian route

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ use App\Http\Controllers\TestController;
 //     return view('welcome');
 // });
 
+// BASIC ROUTING
 // route 1
 Route::get('/hello', function () {
     return 'Hello World';
@@ -33,6 +34,29 @@ Route::get('/', function () {
     return 'Selamat Datang';
 });
 
+// route about
 Route::get('/about', function () {
     return 'Nama: Muhammad Helmi Permana Agung<br>NIM: 2141762140';
+});
+
+// ROUTE PARAMETERS
+// route user
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya '.$name;
+});
+
+// route lebih dari 1 parameter
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+// ID Artikel
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID '.$id;
+});
+
+// OPTIONAL PARAMETER
+// route optional user
+Route::get('/user/{name?}', function ($name='John') {
+   return 'Nama saya '.$name;
 });
